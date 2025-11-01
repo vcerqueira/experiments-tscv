cfc0caafd7836ebde33bc19b658b6ca1b802876a
Vitor Cerqueira <cerqueira.vitormanuel@gmail.com>
2025-11-01 07:26:50 +0000

    kfold

--- src/cv/sw_kfold.py
+++ src/cv/sw_kfold.py
@@ -1,53 +1,31 @@
 from abc import ABC
 
-import numpy as np
-import pandas as pd
-from sklearn.model_selection._split import BaseCrossValidator
+from sklearn.model_selection import KFold, RepeatedKFold
+
+from src.cv.sw_base import SeriesWiseTimeSeriesCV
 
 
-class SeriesWiseTimeSeriesCV(BaseCrossValidator, ABC):
+class SeriesWiseKFold(SeriesWiseTimeSeriesCV, ABC):
+    def __init__(self, n_splits=5, random_state=None, id_col: str = 'unique_id', time_col: str = 'ds'):
+        super().__init__(n_splits=n_splits, id_col=id_col, time_col=time_col)
 
-    def __init__(self, n_splits=None, id_col='unique_id', time_col: str = 'ds'):
-        self.n_splits = n_splits
-        self.id_col = id_col
-        self.time_col = time_col
+        self._kf = KFold(n_splits=n_splits, shuffle=True, random_state=random_state)
 
     def split(self, X, y=None, groups=None):
-        pass
+        yield from self._kf.split(X, y, groups)
 
     def get_n_splits(self, X=None, y=None, groups=None):
-        return self.n_splits
+        return self._kf.get_n_splits(X, y, groups)
 
-    def time_wise_split(self, df: pd.DataFrame, horizon: int):
-        df_by_unq = df.groupby(self.id_col)
 
-        train_l, test_l = [], []
-        for g, df_ in df_by_unq:
-            df_ = df_.sort_values(self.time_col)
+class SeriesWiseRepeatedKFold(SeriesWiseTimeSeriesCV, ABC):
+    def __init__(self, n_splits=5, n_repeats=2, random_state=None, id_col: str = 'unique_id', time_col: str = 'ds'):
+        super().__init__(n_splits=n_splits, id_col=id_col, time_col=time_col)
 
-            train_df_g = df_.head(-horizon)
-            test_df_g = df_.tail(horizon)
+        self._rkf = RepeatedKFold(n_splits=n_splits, n_repeats=n_repeats, random_state=random_state)
 
-            train_l.append(train_df_g)
-            test_l.append(test_df_g)
+    def split(self, X, y=None, groups=None):
+        yield from self._rkf.split(X, y, groups)
 
-        train_df = pd.concat(train_l).reset_index(drop=True)
-        test_df = pd.concat(test_l).reset_index(drop=True)
-
-        return train_df, test_df
-
-    def get_sets_from_idx(self, df: pd.DataFrame, uids: np.ndarray, train_idx: np.ndarray, test_idx: np.ndarray):
-        train_uids, test_uids = uids[train_idx], uids[test_idx]
-
-        # reflect repetitions in train_idx (for bootstrapping)
-        train_df = pd.concat(
-            [df[df[self.id_col] == uid] for uid in train_uids],
-            ignore_index=True
-        )
-
-        test_df = pd.concat(
-            [df[df[self.id_col] == uid] for uid in test_uids],
-            ignore_index=True
-        )
-
-        return train_df, test_df
+    def get_n_splits(self, X=None, y=None, groups=None):
+        return self._rkf.get_n_splits(X, y, groups)
