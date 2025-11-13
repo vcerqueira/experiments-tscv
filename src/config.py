de340e125979703e3b7253c537e8190e839b4741
Vitor Cerqueira <cerqueira.vitormanuel@gmail.com>
2025-11-13 10:10:07 +0000

    prep dry exp

--- src/config.py
+++ src/config.py
@@ -1,9 +1,14 @@
 N_FOLDS = 5
-N_SAMPLES = 5
 SEED = 123
 HOLDOUT_TR = 0.7
 MC_TR = 0.5
 MC_TS = 0.2
 KFOLD_N_REPEATS = 2
-LIMIT_EPOCHS = True
-TRY_MPS = False
+TRY_MPS = True
+DRY_RUN = True
+if DRY_RUN:
+    N_SAMPLES = 2
+    LIMIT_EPOCHS = True
+else:
+    N_SAMPLES = 10
+    LIMIT_EPOCHS = False
