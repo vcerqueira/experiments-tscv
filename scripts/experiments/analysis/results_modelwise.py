e2d3e525fc0b626fbbb2cd029ea79678549f3795
Vitor Cerqueira <cerqueira.vitormanuel@gmail.com>
2026-01-06 14:52:04 +0000

    analysis grouping

--- scripts/experiments/analysis/results_modelwise.py
+++ scripts/experiments/analysis/results_modelwise.py
@@ -12,41 +12,51 @@ from src.mase import mase_scaling_factor
 from src.config import OUT_SET_MULTIPLIER
 from src.cv import CV_METHODS
 
-RESULTS_DIR = "assets/results"
-DATASET = 'monash_tourism_monthly'
+RESULTS_DIR = "assets/results2"
+dataset_names = set(f.split(',')[0] for f in os.listdir(RESULTS_DIR))
+
 MODELS = ["KAN", 'PatchTST', 'NBEATS', 'TFT',
           'TiDE', 'NLinear', "MLP",
           'DLinear', 'NHITS', 'DeepNPTS',
           "SeasonalNaive"]
 
-df, horizon, _, _, seas_len = ChronosDataset.load_everything(DATASET)
-in_set, _ = ChronosDataset.time_wise_split(df, horizon * OUT_SET_MULTIPLIER)
-mase_sf = mase_scaling_factor(seasonality=seas_len, train_df=in_set)
-
-cv_methods = [*CV_METHODS] + ['TimeHoldout']
-
 cv_scores = []
-for method in cv_methods:
-    outer_path = os.path.join(RESULTS_DIR, f"{DATASET},{method},outer.csv")
+for ds in dataset_names:
+    print(ds)
 
-    cv_outer = pd.read_csv(outer_path)
+    df, horizon, _, _, seas_len = ChronosDataset.load_everything(ds)
+    in_set, _ = ChronosDataset.time_wise_split(df, horizon * OUT_SET_MULTIPLIER)
+    mase_sf = mase_scaling_factor(seasonality=seas_len, train_df=in_set)
 
-    radar_outer = ModelRadar(
-        cv_df=cv_outer,
-        metrics=[mae],
-        model_names=MODELS,
-        hardness_reference="SeasonalNaive",
-        ratios_reference="SeasonalNaive",
-    )
+    cv_methods = [*CV_METHODS] + ['TimeHoldout']
 
-    err_outer_uids = radar_outer.evaluate(keep_uids=True)
-    err_outer = err_outer_uids.div(mase_sf, axis=0).mean()
-    err_outer = err_outer.drop('SeasonalNaive')
+    for method in cv_methods:
+        outer_path = os.path.join(RESULTS_DIR, f"{ds},{method},outer.csv")
 
-    scr = {'method': method, **err_outer}
+        cv_outer = pd.read_csv(outer_path)
 
-    cv_scores.append(scr)
+        radar_outer = ModelRadar(
+            cv_df=cv_outer,
+            metrics=[mae],
+            model_names=MODELS,
+            hardness_reference="SeasonalNaive",
+            ratios_reference="SeasonalNaive",
+        )
 
-cv_df = pd.DataFrame(cv_scores).set_index('method')
-print('avg rank')
-print(cv_df.rank().mean(axis=1))
+        err_outer_uids = radar_outer.evaluate(keep_uids=True)
+        err_outer = err_outer_uids.div(mase_sf, axis=0).mean()
+        err_outer = err_outer.drop('SeasonalNaive')
+
+        scr = {'method': method, 'dataset': ds, **err_outer}
+
+        cv_scores.append(scr)
+
+cv_df = pd.DataFrame(cv_scores).set_index(['method', 'dataset'])
+
+ranked_df1 = cv_df.groupby('dataset').rank(axis=0, method='min')
+avg_rank_per_method = ranked_df1.groupby('method').mean().mean(axis=1)
+print(avg_rank_per_method)
+
+ranked_df2 = cv_df.groupby('dataset').rank(axis=1, method='min')
+avg_rank_per_nn = ranked_df2.groupby('method').mean().mean()
+print(avg_rank_per_nn)
