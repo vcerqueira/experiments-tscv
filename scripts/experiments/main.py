6d524e61f1ce09af4eeae58c7ee83ef7a1670602
Vitor Cerqueira <cerqueira.vitormanuel@gmail.com>
2026-02-02 08:40:38 +0000

    add results

--- scripts/experiments/main.py
+++ scripts/experiments/main.py
@@ -14,8 +14,8 @@ warnings.filterwarnings('ignore')
 os.environ['TUNE_DISABLE_STRICT_METRIC_CHECKING'] = '1'
 
 # ---- data loading and partitioning
-target = 'monash_m3_yearly'
-# target = 'm4_yearly'
+target = 'monash_hospital'
+# target = 'monash_hospital'
 df, horizon, _, freq, seas_len = ChronosDataset.load_everything(target)
 # df['unique_id'].value_counts().value_counts().sort_index()
 
