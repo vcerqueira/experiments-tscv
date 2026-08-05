de1da2d9244c814048b997612f481465205cde28
Vitor Cerqueira <cerqueira.vitormanuel@gmail.com>
2026-08-05 08:34:39 +0100

    asha

--- scripts/experiments/main_nestedcv.py
+++ scripts/experiments/main_nestedcv.py
@@ -19,7 +19,7 @@ warnings.filterwarnings('ignore')
 os.environ['TUNE_DISABLE_STRICT_METRIC_CHECKING'] = '1'
 
 # ---- data loading and partitioning
-target = 'ECL'
+target = 'Weather'
 # df, horizon, _, freq, seas_len = ChronosDataset.load_everything(target)
 df, horizon, _, freq, seas_len = LongHorizonDatasetR.load_everything(target, resample_to='D')
 
