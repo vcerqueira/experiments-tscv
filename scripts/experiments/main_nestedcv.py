350dae5bb841c01f75020949323ce2acadfcaef5
Vitor Cerqueira <cerqueira.vitormanuel@gmail.com>
2026-08-31 19:27:06 +0100

    upd exp

--- scripts/experiments/main_nestedcv.py
+++ scripts/experiments/main_nestedcv.py
@@ -19,73 +19,114 @@ warnings.filterwarnings('ignore')
 os.environ['TUNE_DISABLE_STRICT_METRIC_CHECKING'] = '1'
 
 # ---- data loading and partitioning
-target = 'Exchange'
+DATASETS = ['monash_m1_monthly',
+            'monash_m1_quarterly',
+            'monash_m3_monthly',
+            'monash_m3_quarterly',
+            'monash_tourism_monthly',
+            'monash_tourism_quarterly',
+            'monash_hospital',
+            "ECL",
+            "Exchange",
+            "TrafficL",
+            "Weather",]
+LH_DATASETS = ["ECL",
+               "Exchange",
+               "TrafficL",
+               "Weather"]
+# target = 'monash_m3_quarterly'
 # df, horizon, _, freq, seas_len = ChronosDataset.load_everything(target)
-df, horizon, _, freq, seas_len = LongHorizonDatasetR.load_everything(target, resample_to='D')
+# df, horizon, _, freq, seas_len = LongHorizonDatasetR.load_everything(target, resample_to='D')
 
 RESULTS_PATH = '../../assets/results{}'
 
-# - split dataset by time
-# -- estimation_train is used for inner cv and final training
-# ----- the data we use to get performance estimations
-# -- estimation_test is only used at the end to see how well our estimation worked
-in_set_all, out_set = ChronosDataset.time_wise_split(df, horizon * OUT_SET_MULTIPLIER)
-
-if HOLDOUT_FOR_OUTSET > 0:
-    path_ = RESULTS_PATH.format('_holdout')
-    in_set = ChronosDataset.sample_uids(in_set_all, 1 - HOLDOUT_FOR_OUTSET)
-else:
-    path_ = RESULTS_PATH.format('')
-    in_set = in_set_all.copy()
-
-results_dir = Path(path_) / f'seed_{SEED}'
+# # - split dataset by time
+# # -- estimation_train is used for inner cv and final training
+# # ----- the data we use to get performance estimations
+# # -- estimation_test is only used at the end to see how well our estimation worked
+# in_set_all, out_set = ChronosDataset.time_wise_split(df, horizon * OUT_SET_MULTIPLIER)
+#
+# if HOLDOUT_FOR_OUTSET > 0:
+#     path_ = RESULTS_PATH.format('_holdout')
+#     in_set = ChronosDataset.sample_uids(in_set_all, 1 - HOLDOUT_FOR_OUTSET)
+# else:
+#     path_ = RESULTS_PATH.format('')
+#     in_set = in_set_all.copy()
+#
+# results_dir = Path(path_) / f'seed_{SEED}'
 
 if __name__ == '__main__':
-    results_dir.mkdir(parents=True, exist_ok=True)
-    print(results_dir.absolute())
 
-    models = ModelsConfig.get_auto_nf_models(horizon=horizon,
-                                             engine=ENGINE,
-                                             limit_epochs=LIMIT_EPOCHS,
-                                             n_samples=N_SAMPLES)
-
-    tw_outer_path = results_dir / f'{target},TimeHoldout,outer.csv'
-    tw_inner_path = results_dir / f'{target},TimeHoldout,inner.csv'
-
-    if not (tw_outer_path.exists() and tw_inner_path.exists()):
-        print(f"Running cross validation for method: Time-wise Holdout")
-        tw_cv, tw_cv_inner = time_wise_holdout(in_set=in_set,
-                                               in_set_all=in_set_all,
-                                               out_set=out_set,
-                                               freq=freq,
-                                               freq_int=seas_len,
-                                               horizon=horizon,
-                                               models=models,
-                                               out_set_multiplier=OUT_SET_MULTIPLIER)
-
-        tw_cv.to_csv(tw_outer_path, index=False)
-        tw_cv_inner.to_csv(tw_inner_path, index=False)
-    else:
-        print(f"Skipping Time-wise Holdout (results already exist)")
-
-    for method_name in CV_METHODS:
-        cv_outer_path = results_dir / f'{target},{method_name},outer.csv'
-        cv_inner_path = results_dir / f'{target},{method_name},inner.csv'
-
-        if not (cv_outer_path.exists() and cv_inner_path.exists()):
-            print(f"Running cross validation for method: {method_name}")
-            cv_result, cv_inner_result = run_cross_validation(cv_method=method_name,
-                                                              in_set=in_set,
-                                                              in_set_all=in_set_all,
-                                                              out_set=out_set,
-                                                              freq=freq,
-                                                              freq_int=seas_len,
-                                                              horizon=horizon,
-                                                              nf_models=models,
-                                                              random_state=SEED,
-                                                              out_set_multiplier=OUT_SET_MULTIPLIER)
-
-            cv_result.to_csv(cv_outer_path, index=False)
-            cv_inner_result.to_csv(cv_inner_path, index=False)
+    for target in DATASETS:
+        print(f"Running {target}")
+        if target in LH_DATASETS:
+            df, horizon, _, freq, seas_len = LongHorizonDatasetR.load_everything(target, resample_to='D')
         else:
-            print(f"Skipping {method_name} (results already exist)")
+            df, horizon, _, freq, seas_len = ChronosDataset.load_everything(target)
+
+        # - split dataset by time
+        # -- estimation_train is used for inner cv and final training
+        # ----- the data we use to get performance estimations
+        # -- estimation_test is only used at the end to see how well our estimation worked
+        in_set_all, out_set = ChronosDataset.time_wise_split(df, horizon * OUT_SET_MULTIPLIER)
+
+        if HOLDOUT_FOR_OUTSET > 0:
+            path_ = RESULTS_PATH.format('_holdout')
+            in_set = ChronosDataset.sample_uids(in_set_all, 1 - HOLDOUT_FOR_OUTSET)
+        else:
+            path_ = RESULTS_PATH.format('')
+            in_set = in_set_all.copy()
+
+        results_dir = Path(path_) / f'seed_{SEED}'
+
+        results_dir.mkdir(parents=True, exist_ok=True)
+        print(results_dir.absolute())
+
+
+        # ---
+
+        models = ModelsConfig.get_auto_nf_models(horizon=horizon,
+                                                 engine=ENGINE,
+                                                 limit_epochs=LIMIT_EPOCHS,
+                                                 n_samples=N_SAMPLES)
+
+        tw_outer_path = results_dir / f'{target},TimeHoldout,outer.csv'
+        tw_inner_path = results_dir / f'{target},TimeHoldout,inner.csv'
+
+        if not (tw_outer_path.exists() and tw_inner_path.exists()):
+            print(f"Running cross validation for method: Time-wise Holdout")
+            tw_cv, tw_cv_inner = time_wise_holdout(in_set=in_set,
+                                                   in_set_all=in_set_all,
+                                                   out_set=out_set,
+                                                   freq=freq,
+                                                   freq_int=seas_len,
+                                                   horizon=horizon,
+                                                   models=models,
+                                                   out_set_multiplier=OUT_SET_MULTIPLIER)
+
+            tw_cv.to_csv(tw_outer_path, index=False)
+            tw_cv_inner.to_csv(tw_inner_path, index=False)
+        else:
+            print(f"Skipping Time-wise Holdout (results already exist)")
+
+        for method_name in CV_METHODS:
+            cv_outer_path = results_dir / f'{target},{method_name},outer.csv'
+            cv_inner_path = results_dir / f'{target},{method_name},inner.csv'
+
+            if not (cv_outer_path.exists() and cv_inner_path.exists()):
+                print(f"Running cross validation for method: {method_name}")
+                cv_result, cv_inner_result = run_cross_validation(cv_method=method_name,
+                                                                  in_set=in_set,
+                                                                  in_set_all=in_set_all,
+                                                                  out_set=out_set,
+                                                                  freq=freq,
+                                                                  freq_int=seas_len,
+                                                                  horizon=horizon,
+                                                                  nf_models=models,
+                                                                  random_state=SEED,
+                                                                  out_set_multiplier=OUT_SET_MULTIPLIER)
+
+                cv_result.to_csv(cv_outer_path, index=False)
+                cv_inner_result.to_csv(cv_inner_path, index=False)
+            else:
+                print(f"Skipping {method_name} (results already exist)")
